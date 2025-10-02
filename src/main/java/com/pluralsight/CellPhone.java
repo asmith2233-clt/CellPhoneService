f4a4e1a10453c18584af6b85e8b7b54ba47867d5
asmith2233 <ayannasmith2233@gmail.com>
2025-10-02 14:07:43 -0400

    finished files

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -1,7 +1,83 @@
 package com.pluralsight;
 
 public class CellPhone {
-    public static void main(String[] args){
-        
+    private int serialNumber;
+    private String model;
+    private String carrier;
+    private String phoneNumber;
+    private String owner;
+
+    public CellPhone() {
+        serialNumber = 0;
+        model = "";
+        carrier = "";
+        phoneNumber = "";
+        owner = "";
+
     }
+    public CellPhone(int serialNumber,String model,String carrier,String phoneNumber, String owner){
+        this.serialNumber = serialNumber;
+        this.model = model;
+        this.carrier = carrier;
+        this.phoneNumber = phoneNumber;
+        this.owner = owner;
+
+    }
+
+    public int getSerialNumber() {
+        return this.serialNumber;
+
+    }
+
+    public void setSerialNumber(int newSerialNumber) {
+        //this line of code...
+        this.serialNumber = serialNumber;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getCarrier() {
+        return this.carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getOwner() {
+        return this.owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+
+    }
+
+    public void dial(String phoneNumberToCall) {
+        System.out.println(owner + "'s Phone(" + phoneNumber + ") is calling" + phoneNumberToCall);
+        System.out.println("Ring?");
+        System.out.println("Ring?");
+        System.out.println("Ring?");
+
+    }
+
+    public void dial(CellPhone phone) {
+        dial(phone.getPhoneNumber());
+
+    }
+
 }
--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,7 +1,58 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class CellPhoneApplication {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+            CellPhone myPhone = new CellPhone();
 
+            System.out.println("What is the serial number?");
+            int serialNumber = scanner.nextInt();
+            scanner.nextLine();
+
+            myPhone.setSerialNumber(serialNumber);
+
+            //model
+        System.out.print("What is the model?: " );
+        String model = scanner.nextLine();
+
+        myPhone.setModel(model);
+
+            //carrier
+        System.out.print("What is the carrier?: " );
+        String carrier = scanner.nextLine();
+
+        myPhone.setCarrier(carrier);
+
+            //phone number
+        System.out.print("What is the phone number?: ");
+        String phoneNumber = scanner.nextLine();
+
+        myPhone.setPhoneNumber(phoneNumber);
+
+            //owner
+        System.out.print("What is the owner?: ");
+        String owner = scanner.nextLine();
+
+        myPhone.setOwner(owner);
+
+        //display it
+        display(myPhone);
+
+        myPhone.dial("555-555-5555");
+        CellPhone yourPhone = new CellPhone(1234,
+                "pixel",
+                "fi",
+                "888-888-8888",
+                "ayanna");
+
+    }
+    private static void display(CellPhone phone){
+        System.out.println("----------------");
+        System.out.println("CELL PHONE [SN:" + phone.getSerialNumber()+"]");
+        System.out.println("Owner:" + phone.getOwner());
+        System.out.println("Model:" + phone.getModel());
+        System.out.println("Carrier:" + phone.getCarrier());
     }
 }
